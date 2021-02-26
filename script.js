28dac6b58c35cbf393aed3cbd16e90f72a1bb51b
ifteASC4 <ifteahmed2000@gmail.com>
2021-02-25 20:14:38 -0500

    took updates

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-let numRows = 0;
-let numCols = 0;
+let numRows = 3;
+let numCols = 3;
 let colorSelected;
 
 function selcted(){
 	colorSelected = document.getElementById("selctedID").value;
 	console.log(colorSelected);
+}
+
+function removeR(){ //ifte is working on this
+	alert("testing");	
 }
