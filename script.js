2cbbebf4b87addaa45ab2cd9f734a12bcafc042b
jamyooes <james.yoo715@gmail.com>
2021-02-25 20:40:50 -0500

    added comments to the functions

--- script.js
+++ script.js
@@ -21,32 +21,37 @@ function addR()
 	//checking that the rows were being added
 	//console.log("clicked add row", numRows);
 }
+
+//adding the fill() function
+//done by James Yoo
 function fill()
 {
-	let original_box= document.getElementById("grid");
-	let trs=original_box.getElementsByTagName("tr");
+	let original_box= document.getElementById("grid");	//access the grid
+	let trs=original_box.getElementsByTagName("tr");	//get all the elements of tr
 	
-	for (let i=0; i<numRows; i++)
+	for (let i=0; i<numRows; i++)						//loop through to go through tows and columns
 	{
-		let tds= trs[i].getElementsByTagName("td");
+		let tds= trs[i].getElementsByTagName("td");		//to access the columns of the boxes in each row
 		for (let j=0; j<numCols; j++)
 		{
-			tds[j].style.backgroundColor=colorSelected;
+			tds[j].style.backgroundColor=colorSelected; //change color to color Selected
 		}
 	}
 }
 
+//clearAll() function
+//done by James Yoo
 function clearAll()
 {
-	let original_box= document.getElementById("grid");
-	let trs=original_box.getElementsByTagName("tr");
+	let original_box= document.getElementById("grid");	//access the grid
+	let trs=original_box.getElementsByTagName("tr");	//get all the elements of tr
 	
-	for (let i=0; i<numRows; i++)
+	for (let i=0; i<numRows; i++)						//loop through the rows and columns
 	{
-		let tds= trs[i].getElementsByTagName("td");
+		let tds= trs[i].getElementsByTagName("td");		//to access the columns of the boxes in each row
 		for (let j=0; j<numCols; j++)
 		{
-			tds[j].style.backgroundColor="white";
+			tds[j].style.backgroundColor="white";		//reset the color to white.
 		}
 	}
 }
