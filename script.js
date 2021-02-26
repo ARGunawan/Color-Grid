ae4742c698179720442ca72077bf74c7e464b5ae
jamyooes <james.yoo715@gmail.com>
2021-02-25 20:26:31 -0500

    messed up again

--- script.js
+++ script.js
@@ -21,18 +21,3 @@ function fill()
 		}
 	}
 }
-
-function clearAll()
-{
-	let original_box= document.getElementById("grid");
-	let trs=original_box.getElementsByTagName("tr");
-	
-	for (let i=0; i<numRows; i++)
-	{
-		let tds= trs[i].getElementsByTagName("td");
-		for (let j=0; j<numCols; j++)
-		{
-			tds[j].style.backgroundColor="white";
-		}
-	}
-}
