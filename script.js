ece188e4ec2fb21a4916d9c58107ece6e48e6096
Caitlin Sangcap <csangcap405@gmail.com>
2021-02-25 20:31:00 -0500

    addR() is working

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ function selcted(){
 	colorSelected = document.getElementById("selctedID").value;
 	console.log(colorSelected);
 }
+
+//adding the addR() function
+//done by Caitlin-Dawn Sangcap
+function addR() 
+{
+	var table = document.getElementById("grid");
+	let row = document.createElement("TR");
+	var cell = document.createElement("TD");
+	row.append(cell);
+	table.append(row);
+	numRows++;
+	//checking that the rows were being added
+	//console.log("clicked add row", numRows);
+}
